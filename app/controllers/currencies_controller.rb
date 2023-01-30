c3d3adbd123205dcc67f80b3f5e9e0c90670367a
lvmadoreira <lvaladao@chicagobooth.edu>
2023-01-30 06:29:50 +0000

    Foreign Exchange Rates Done

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -11,4 +11,12 @@ class CurrenciesController < ActionController::Base
     
     render({ :template => "currency_templates/step_two.html.erb" })
   end
+
+  def third_currency
+    @from_currency = params.fetch("from_currency")
+    @to_currency = params.fetch("to_currency")
+    @rate = JSON.parse(open("https://api.exchangerate.host/convert?from=" + @from_currency + "&to=" + @to_currency).read).fetch("info").fetch("rate")
+    
+    render({ :template => "currency_templates/step_three.html.erb" })
+  end
 end
